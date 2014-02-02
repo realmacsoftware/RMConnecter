65ffd688c74b17a840a8d4d9b6b875042c14675f
Damien DeVille <d.deville@me.com>
2014-02-02 16:15:08 +0000

    New category to add keychain support to the credentials object.

---
+++ Connecter/RMConnecterCredentials+Keychain.h
@@ -0,0 +1,13 @@
+//
+//  RMConnecterCredentials+Keychain.h
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import "RMConnecterCredentials.h"
+
+@interface RMConnecterCredentials (Keychain)
+
+@end
---
+++ Connecter/RMConnecterCredentials+Keychain.m
@@ -0,0 +1,13 @@
+//
+//  RMConnecterCredentials+Keychain.m
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import "RMConnecterCredentials+Keychain.h"
+
+@implementation RMConnecterCredentials (Keychain)
+
+@end
