7f1d12589fa380748701a45827f4e9d53a30f6f5
Damien DeVille <d.deville@me.com>
2014-02-02 13:07:48 +0000

    create a window controller to manage the main window rather that having everything in the application delegate

---
+++ Connecter/RMConnecterWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RMConnecterWindowController.h
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMConnecterWindowController : NSWindowController
+
+@end
---
+++ Connecter/RMConnecterWindowController.m
@@ -0,0 +1,22 @@
+//
+//  RMConnecterWindowController.m
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import "RMConnecterWindowController.h"
+
+@interface RMConnecterWindowController ()
+
+@end
+
+@implementation RMConnecterWindowController
+
+- (id)init
+{
+	return [self initWithWindowNibName:@"RMConnecterWindow" owner:self];
+}
+
+@end
