9917394eb5479eee30e0ad23b410f102bca10c2b
Damien DeVille <d.deville@me.com>
2014-02-02 14:47:10 +0000

    New object to store the credentials.

---
+++ Connecter/RMConnecterCredentials.h
@@ -0,0 +1,16 @@
+//
+//  RMConnecterCredentials.h
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMConnecterCredentials : NSObject
+
+@property (copy, nonatomic) NSString *username;
+@property (copy, nonatomic) NSString *password;
+
+@end
---
+++ Connecter/RMConnecterCredentials.m
@@ -0,0 +1,13 @@
+//
+//  RMConnecterCredentials.m
+//  Connecter
+//
+//  Created by Damien DeVille on 2/2/14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import "RMConnecterCredentials.h"
+
+@implementation RMConnecterCredentials
+
+@end
