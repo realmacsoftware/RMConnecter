12b2f692729d23f529388e5f8cb1349a5c39a033
Markus Emrich <Markus.Emrich@gmail.com>
2014-02-14 16:21:17 +0100

    added md5 helper

---
+++ Connecter/NSData+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSData+MD5.h
+//  Connecter
+//
+//  Created by Markus on 14.02.14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+
+- (NSString*)md5CheckSum;
+
+@end
