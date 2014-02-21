44c66ed3057fec6d2eb5740d10a88b23e43df209
Markus Emrich <Markus.Emrich@gmail.com>
2014-02-21 12:58:40 +0100

    correctly format image size in b/kb/MB

---
+++ Connecter/RMByteValueTransformer.h
@@ -0,0 +1,13 @@
+//
+//  RMByteValueTransformer.h
+//  Connecter
+//
+//  Created by Markus on 21.02.14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMByteValueTransformer : NSValueTransformer
+
+@end
---
+++ Connecter/RMByteValueTransformer.m
@@ -0,0 +1,38 @@
+//
+//  RMByteValueTransformer.m
+//  Connecter
+//
+//  Created by Markus on 21.02.14.
+//  Copyright (c) 2014 Realmac Software. All rights reserved.
+//
+
+#import "RMByteValueTransformer.h"
+
+@implementation RMByteValueTransformer
+
+- (id)transformedValue:(id)value;
+{
+    if ([value isKindOfClass:[NSNumber class]]) {
+        CGFloat newValue = [value floatValue];
+        
+        if (newValue > 1000000) {
+            newValue /= 1000000;
+            return [NSString stringWithFormat: @"%.1f MB", newValue];
+        }
+        
+        if (newValue > 1000) {
+            newValue /= 1000;
+            return [NSString stringWithFormat: @"%.0f kb", newValue];
+        }
+        
+        return [NSString stringWithFormat: @"%.0f b", newValue];
+    }
+    return nil;
+}
+
++ (Class)transformedValueClass;
+{
+    return [NSString class];
+}
+
+@end
